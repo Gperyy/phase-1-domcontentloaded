7a617d46e0049d4af56f4eb7b4dcf554fb93a349
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-04-25 15:09:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// Your code goes here
+document.addEventListener("DOMContentLoaded", () =>
+    console.log("The DOM has Loaded")
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let p = document.querySelector("p");
+    p.textContent = "This is really cool!";
+});
